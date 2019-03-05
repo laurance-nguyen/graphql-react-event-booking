d0f59d5da58b3df66574c166476052ed8c810b97
Shogun Ross <nguyenconglong95@gmail.com>
2019-03-05 10:40:13 +0700

    * Ep #4

--- src/app.js
+++ src/app.js
@@ -10,17 +10,34 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
+const events = [];
+
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
+    type Event{
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -30,11 +47,18 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return ['Romantic Cooking', 'Sailing', 'All-Night Coding', 'Twiceland'];
+      return events;
     },
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: args.eventInput.date
+      };
+      events.push(event)
+      return event
     }
   },
   graphiql: true
