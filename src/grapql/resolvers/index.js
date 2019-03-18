8111b015991281191a7c7650e1b95b36876d269a
ShogunRoss <nguyenconglong95@gmail.com>
2019-03-18 21:56:03 +0700

    *Ep #8

--- src/grapql/resolvers/index.js
+++ src/grapql/resolvers/index.js
@@ -1,5 +1,6 @@
 const Event = require('../../models/event');
 const User = require('../../models/user');
+const Booking = require('../../models/booking');
 
 const bcrypt = require('bcryptjs');
 
@@ -16,6 +17,19 @@ const events = async eventIds => {
     });
   }
   catch (err) {
+    throw err;
+  }
+}
+
+const singleEvent = async eventId => {
+  try {
+    const event = await Event.findById(eventId);
+    return {
+      ...event._doc,
+      _id: event.id,
+      creator: user.bind(this, event.creator),
+    }
+  } catch (err) {
     throw err;
   }
 }
@@ -52,15 +66,32 @@ module.exports = {
       throw err;
     };
   },
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find()
+      return bookings.map(booking => {
+        return {
+          ...booking._doc,
+          _id: booking.id,
+          user: user.bind(this, booking._doc.user),
+          event: singleEvent.bind(this, booking._doc.event),
+          createdAt: new Date(booking._doc.createdAt).toISOString(),
+          updatedAt: new Date(booking._doc.updatedAt).toISOString(),
+        };
+      });
+    }
+    catch (err) {
+      throw err;
+    }
+  },
   createEvent: async args => {
     const event = new Event({
       title: args.eventInput.title,
       description: args.eventInput.description,
       price: +args.eventInput.price,
       date: new Date(args.eventInput.date),
-      creator: '5c8b532842f31c3534d14033'
+      creator: '5c8e6f836b0af4150c8c618f'
     });
-
     let createdEvent;
     try {
       const result = await event.save();
@@ -70,7 +101,7 @@ module.exports = {
         date: new Date(result._doc.date).toISOString(),
         creator: user.bind(this, result._doc.creator),
       };
-      const creator = await User.findById('5c8b532842f31c3534d14033');
+      const creator = await User.findById('5c8e6f836b0af4150c8c618f');
       if (!creator) {
         throw new Error('User not found.');
       }
@@ -96,8 +127,38 @@ module.exports = {
       const result = await user.save();
       return { ...result._doc, password: null, _id: result.id };
     }
-    catch(err) {
+    catch (err) {
       throw err;
     };
+  },
+  bookEvent: async args => {
+    const fetchedEvent = await Event.findOne({ _id: args.eventId });
+    const booking = new Booking({
+      user: '5c8e6f836b0af4150c8c618f',
+      event: fetchedEvent,
+    });
+    const result = await booking.save();
+    return {
+      ...result._doc,
+      _id: result.id,
+      user: user.bind(this, result._doc.user),
+      event: singleEvent.bind(this, result._doc.event),
+      createdAt: new Date(result._doc.createdAt).toISOString(),
+      updatedAt: new Date(result._doc.updatedAt).toISOString(),
+    }
+  },
+  cancelBooking: async args => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate('event');
+      const event = {
+        ...booking.event._doc,
+        _id: booking.event.id,
+        creator: user.bind(this, booking.event._doc.creator),
+      }
+      await Booking.deleteOne({ _id: args.bookingId });
+      return event;
+    } catch (err) {
+      throw err;
+    }
   }
-}
+};
