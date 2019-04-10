d49a7ecc79f4edff6ac05afa211473c3157b0fe5
ShogunRoss <nguyenconglong95@gmail.com>
2019-04-10 21:47:31 +0700

    * Change to ubuntu dev

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -2,7 +2,22 @@ import React, {Component} from 'react';
 
 class AuthPage extends Component {
   render() {
-    return(<h1>The Auth Page</h1>);
+    return (
+      <form>
+        <div className="form-control">
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email"/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div>
+          <button type="button">Switch to Signup</button>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    );
   }
 }
 
